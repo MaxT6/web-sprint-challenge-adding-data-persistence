24f81daff4754a8ad17601e8b4d97686d26ef2f6
MaxT6 <maximiliant6@gmail.com>
2022-06-22 19:13:30 -0400

    get for resource

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,17 @@
 // build your `/api/resources` router here
+const express = require('express');
+const router = express.Router();
+const Resource = require('./model')
+
+router.get('/', async (req, res, next) => {
+  try {
+    const resource = await Resource.locate()
+    res.json(resource)
+  }
+  catch(err) {
+    next(err)
+  }
+  
+})
+
+module.exports = router;
